d1d8c7b9fcdb80b626ac2757c305fdfdbf2e0cc0
Abdul Basit <dev.basit2020@gmail.com>
2024-05-12 18:50:20 +0500

    Added: user authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Header from "@/components/header/Header";
+import Providers from "@/components/Providers";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          {children}
-          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-            All Rights Reserved - <>{metadata.title}</>
-          </footer>
-        </div>
+        <Providers>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            {children}
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+              <p>
+                Copyright © <>{new Date().getFullYear()}</> - Kunar Electro
+              </p>
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
